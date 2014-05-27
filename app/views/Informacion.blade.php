a2c4df0657098f2706957ba90dc1fa8ce3d0ceb8
jamthebest <javier.midence@unitec.edu>
2014-05-27 15:52:38 -0600

    Implementando Carousel

--- app/views/Informacion.blade.php
+++ app/views/Informacion.blade.php
@@ -6,16 +6,54 @@
 	<div class="col-md-3"><h2>Información > <small></small></h2></div>
 </div>
 
-<div class="row" style="margin-top:5%;">
+<div class="row" style="margin-top:1%;">
 	<div class="col-md-10 col-md-offset-1">
 		<h2>XXXXX es un servicio innovador de consultorías donde se encuentran profesionales altamente calificados y preparados, 
 		que proveen soluciones inteligentes	a empresas privadas o públicas.<br><br> XXXXX abarca una amplia cantidad de áreas, 
 		dentro de las cuales cada una cuenta con docentes altamente calificados y especializados, para brindar atención personalizada 
 		y lograr su satisfacción.</h2>
 	</div>
-	<div class="col-md-3 col-md-push-1"><img src="images/icon.png" height="200"></div>
-	<div class="col-md-3 col-md-push-1"><img src="images/icon.png" height="200"></div>
-	<div class="col-md-3 col-md-push-1"><img src="images/icon.png" height="200"></div>
+	<div class="col-md-6" style="margin-top:2%; width:400px;height:400px;">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        </ol>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+          <div class="active item"><img  src="images/35.jpg" alt="banner1" /></div>
+          <div class="item"><img  src="images/4.jpeg" alt="banner2" /></div>
+        </div>
+        <!-- Carousel nav -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+      </div>
+  </div>
+
+  <div class="col-md-6" style="margin-top:2%; width:400px;height:400px;">
+      <div id="carousel-example-generic2" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+        </ol>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+          <div class="active item"><img  src="images/2.jpeg" alt="banner1" /></div>
+          <div class="item"><img  src="images/10.jpeg" alt="banner2" /></div>
+        </div>
+        <!-- Carousel nav -->
+        <a class="left carousel-control" href="#carousel-example-generic2" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+        <a class="right carousel-control" href="#carousel-example-generic2" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+      </div>
+  </div>
 </div>
 
 @stop
--- app/views/Inicio.blade.php
+++ app/views/Inicio.blade.php
@@ -14,7 +14,31 @@
 @endif
 
 <div class="row">
-	<div class="col-md-6"><img src="images/icon.png" height="400"></div>
+	<div class="col-md-6 span6" style="margin-top:10%; width:400px;height:400px;">
+      <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+        </ol>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
+          <div class="active item"><img  src="images/24.jpg" alt="banner1" /></div>
+          <div class="item"><img  src="images/8.jpg" alt="banner2" /></div>
+          <div class="item"><img  src="images/22.jpg" alt="banner3" /></div>
+          <div class="item"><img  src="images/21.jpg" alt="banner4" /></div>
+        </div>
+        <!-- Carousel nav -->
+        <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left"></span>
+			  </a>
+        <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right"></span>
+			  </a>
+      </div>
+  </div>
+	
 	<div class="col-md-6" style="margin-top:2%;">
 		
 		<h2>Para mejorar el ambiente laboral del país, Unitec ha creado "XXXXX", un sevicio de asesoría para las empresas del sector público y privado</h2>
