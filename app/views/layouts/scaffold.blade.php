7c189d8dc3ed0a9db4a09a808076f78064061280
jamthebest <javier.midence@unitec.edu>
2014-05-26 19:19:30 -0600

    Error en route de tercer nivel corregido

--- app/views/layouts/scaffold.blade.php
+++ app/views/layouts/scaffold.blade.php
@@ -3,66 +3,50 @@
 <head>
   <meta charset="UTF-8">
   <title>XXXXX</title>
-  <link rel="stylesheet" href="<?php public_path(); ?>/bootstrap/css/bootstrap.min.css">
-
-    <!--script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script-->
-
-  <link rel="stylesheet" type="text/css" href="/assets/css/general.css">
   <script type="text/javascript" src="/assets/javascript/script.js"></script>
   <script type="text/javascript" src="/assets/javascript/datetimepicker.js"></script>
-
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-  <link href="css/main.css" rel="stylesheet">
   <script type="text/javascript" src="js/jquery.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
-
-    <!-- Bootstrap -->
-    <!--<link href="css/bootstrap.min.css" rel="stylesheet">
-
-  <script src="<?php public_path(); ?>/bootstrap/js/jquery-2.0.2.min.js"></script>
-  <script src="<?php public_path(); ?>/bootstrap/js/bootstrap.min.js"></script> 
-  -->
-    <!--script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
-  <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script-->
-
+  {{HTML::style('/bootstrap/css/bootstrap.min.css');}}
+  {{HTML::style('/assets/css/general.css');}}
+  {{HTML::style('/bootstrap/css/css/bootstrap.min.css');}}
+  {{HTML::style('/css/main.css');}}
 </head>
 <body style="margin-top: 50px">
   <header>
       <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-        <a class="navbar-brand" href="Inicio"> <img src="images/icon.png"> XXXXX</a>
+        <a class="navbar-brand" href="Inicio"> {{HTML::image('/images/icon.png');}} XXXXX</a>
         <ul class="nav navbar-nav navbar-left">
           <li class="dropdown">
-            <a href='Inicio' class="dropdown-toggle">Inicio</a>
+            {{link_to('Inicio', 'Inicio', $attributes = array(), $secure = null)}}
           </li>
 
           <li class="dropdown">
-            <a href='Informacion' class="dropdown-toggle">Información </a>
+            {{link_to('Informacion', 'Información', $attributes = array(), $secure = null)}}
           </li>
 
           <li class="dropdown">
-            <a href="Categorias" class="dropdown-toggle">Categorías de Interés</a>
+            {{link_to('Categorias', 'Categorías de Interés', $attributes = array(), $secure = null)}}
           </li>
 
           <li class="dropdown">
-            <a href="Ejemplos" class="dropdown-toggle">Ejemplos de Casos</a>
+            {{link_to('Ejemplos', 'Ejemplos de Casos', $attributes = array(), $secure = null)}}
           </li>
 
           <li class="dropdown">
-            <a href="Solicitudes" class="dropdown-toggle">Solicitudes</a>
+            {{link_to('Solicitudes', 'Solicitudes', $attributes = array(), $secure = null)}}
           </li>
 
           <li class="dropdown">
-            <a href="Comentarios" class="dropdown-toggle">Comentarios</a>
+            {{link_to('Comentarios', 'Comentarios', $attributes = array(), $secure = null)}}
           </li>
         </ul>
         @if (Auth::user())
-            <p class="navbar-text navbar-right" style="margin-right: 1em;"><a href="Logout" class="navbar-link">Salir</a></p>
-            <p class="navbar-text navbar-right" style="margin-right: 1em;"><a href="Usuarios/{{Auth::user()->id}}" class="navbar-link">{{{Auth::user()->user}}}</a></p>
+            <p class="navbar-text navbar-right" style="margin-right: 1em;">{{link_to('Logout', 'Salir', $attributes = array(), $secure = null)}}</p>
+            <p class="navbar-text navbar-right" style="margin-right: 1em;">{{link_to('Usuarios/'.Auth::user()->id, Auth::user()->user, $attributes = array(), $secure = null)}}</p>
         @else
-            <p class="navbar-text navbar-right" style="margin-right: 1em;"><a href="Registro" class="navbar-link">Registrarse</a></p>
-            <p class="navbar-text navbar-right" style="margin-right: 1em;"><a href="Login" class="navbar-link">Ingresar</a></p>
+            <p class="navbar-text navbar-right" style="margin-right: 1em;">{{link_to('Registro', 'Registrarse', $attributes = array(), $secure = null)}}</p>
+            <p class="navbar-text navbar-right" style="margin-right: 1em;">{{link_to('Login', 'Ingresar', $attributes = array(), $secure = null)}}</p>
         @endif
       </nav>
     </header>
